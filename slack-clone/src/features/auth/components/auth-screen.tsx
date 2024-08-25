2bdeb95e71acfd94f8dd017279568b7e42fa1892
Michael Van <michaelvan996@gmail.com>
2024-08-25 12:23:02 -0400

    updated SignInCard Component

--- slack-clone/src/features/auth/components/auth-screen.tsx
+++ slack-clone/src/features/auth/components/auth-screen.tsx
@@ -3,6 +3,8 @@
 import { useState } from "react";
 
 import { SignInFlow } from "../types";
+import { SignInCard } from "./sign-in-card";
+import { SignUpCard } from "./sign-up-card";
 
 export const AuthScreen = () => {
     const [state, setState] = useState<SignInFlow>("signIn");
@@ -10,7 +12,7 @@ export const AuthScreen = () => {
     return (
         <div className="h-full flex items-center justify-center bg-[#5C3B58]">
             <div className="md:h-auto md:w-[420px]">
-                Auth screen
+                {state === "signIn" ? <SignInCard /> : <SignUpCard />}
             </div>
         </div>
     );
