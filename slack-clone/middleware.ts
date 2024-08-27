4129149ac78b25ef16111e99f371d1b8535257b3
Michael Van <michaelvan996@gmail.com>
2024-08-26 22:28:01 -0400

    update authentication middleware

--- slack-clone/middleware.ts
+++ slack-clone/middleware.ts
@@ -1,6 +1,17 @@
-import { convexAuthNextjsMiddleware } from "@convex-dev/auth/nextjs/server";
+import { 
+    convexAuthNextjsMiddleware, 
+    createRouteMatcher, 
+    isAuthenticatedNextjs,
+    nextjsMiddlewareRedirect
+} from "@convex-dev/auth/nextjs/server";
+
+const isPublicPage = createRouteMatcher(["/signin"]);
  
-export default convexAuthNextjsMiddleware();
+export default convexAuthNextjsMiddleware((request) => {
+    if (!isPublicPage(request) && !isAuthenticatedNextjs()) {
+        return nextjsMiddlewareRedirect(request, "/signin");
+    }
+});
  
 export const config = {
   // The following matcher runs middleware on all routes
