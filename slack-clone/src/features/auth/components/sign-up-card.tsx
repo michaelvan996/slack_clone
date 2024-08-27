b7414813ad3325a30cbc861dc7ea274e027566a5
Michael Van <michaelvan996@gmail.com>
2024-08-27 18:36:34 -0400

    updating sign up component

--- slack-clone/src/features/auth/components/sign-up-card.tsx
+++ slack-clone/src/features/auth/components/sign-up-card.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
+import { TriangleAlert } from "lucide-react";
+import { useAuthActions } from "@convex-dev/auth/react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -14,14 +16,27 @@ import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import { SignInFlow } from "../types";
 
+
 interface SignUpCardProps {
     setState: (state: SignInFlow) => void;
 };
 
 export const SignUpCard = ({ setState }: SignUpCardProps) => {
+    const { signIn } = useAuthActions();
+
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
+    const [error, setError] = useState("");
+    const [pending, setPending] = useState(false);
+
+    const onProviderSignUp = (value: "github" | "google") => {
+        setPending(true);
+        signIn(value)
+            .finally(() => {
+                setPending(false);
+            })
+    };
 
     return (
         <Card className="w-full h-full p-8">
@@ -33,7 +48,12 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
                     Use your email or another service to continue
                 </CardDescription>
             </CardHeader>
-            
+            {!!error && (
+                <div className="bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive mb-6">
+                    <TriangleAlert className="size-4" />
+                    <p>{error}</p>
+                </div>
+            )}
             <CardContent className="space-y-5 px-0 pb-0">
                 <form className="space-y-2.5">
                     <Input
@@ -68,7 +88,7 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
                 <div className="flex flex-col gap-y-2.5">
                     <Button
                         disabled={false}
-                        onClick={() => {}}
+                        onClick={() => onProviderSignUp("google")}
                         variant="outline"
                         size="lg"
                         className="w-full relative"
@@ -78,7 +98,7 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
                     </Button>
                     <Button
                         disabled={false}
-                        onClick={() => {}}
+                        onClick={() => onProviderSignUp("github")}
                         variant="outline"
                         size="lg"
                         className="w-full relative"
