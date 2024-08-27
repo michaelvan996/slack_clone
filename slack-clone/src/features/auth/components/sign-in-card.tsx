2a114baeccd020159d647957d72b6136e6da4aaa
Michael Van <michaelvan996@gmail.com>
2024-08-26 23:22:12 -0400

    add github authentication

--- slack-clone/src/features/auth/components/sign-in-card.tsx
+++ slack-clone/src/features/auth/components/sign-in-card.tsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
+import { useAuthActions } from "@convex-dev/auth/react";
+
 
 import { Button } from "@/components/ui/button";
 import {
-    Card, 
-    CardContent, 
-    CardDescription, 
-    CardHeader, 
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
     CardTitle
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
@@ -19,9 +21,14 @@ interface SignInCardProps {
 };
 
 export const SignInCard = ({ setState }: SignInCardProps) => {
+    const { signIn } = useAuthActions();
+
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    const handleProviderSignIn = (value: "github" | "google") => {
+        signIn(value);
+    };
 
     return (
         <Card className="w-full h-full p-8">
@@ -33,7 +40,7 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
                     Use your email or another service to continue
                 </CardDescription>
             </CardHeader>
-            
+
             <CardContent className="space-y-5 px-0 pb-0">
                 <form className="space-y-2.5">
                     <Input
@@ -60,7 +67,7 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
                 <div className="flex flex-col gap-y-2.5">
                     <Button
                         disabled={false}
-                        onClick={() => {}}
+                        onClick={() => { }}
                         variant="outline"
                         size="lg"
                         className="w-full relative"
@@ -70,7 +77,7 @@ export const SignInCard = ({ setState }: SignInCardProps) => {
                     </Button>
                     <Button
                         disabled={false}
-                        onClick={() => {}}
+                        onClick={() => handleProviderSignIn("github")}
                         variant="outline"
                         size="lg"
                         className="w-full relative"
