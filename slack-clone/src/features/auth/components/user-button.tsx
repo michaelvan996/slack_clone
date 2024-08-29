08209925d527975b5346765dd9c83de8577ac1cd
Michael Van <michaelvan996@gmail.com>
2024-08-28 21:11:51 -0400

    updated user-button component

--- slack-clone/src/features/auth/components/user-button.tsx
+++ slack-clone/src/features/auth/components/user-button.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { Loader, LogOut } from "lucide-react";
+import { useAuthActions } from "@convex-dev/auth/react";
+
 import { 
     Avatar, 
     AvatarFallback, 
@@ -12,20 +15,38 @@ import {
     DropdownMenuTrigger,
   } from "@/components/ui/dropdown-menu";
 
+import { useCurrentUser } from "../hooks/use-current-user";
+
   export const UserButton = () => {
+    const { signOut } = useAuthActions();
+    const { data, isLoading } = useCurrentUser();
+
+    if (isLoading) {
+        return <Loader className="size-4 animate-spin text-muted-foreground" />
+    }
+
+    if (!data) {
+        return null;
+    }
+
+    const { image, name } = data;
+
+    const avatarFallback = name!.charAt(0).toUpperCase();
+
     return (
         <DropdownMenu modal={false}>
             <DropdownMenuTrigger className="outline-none relative">
                 <Avatar className="size-10 hover:opacity-75 transition">
-                    <AvatarImage />
-                    <AvatarFallback>
-
+                    <AvatarImage alt={name} src={image} />
+                    <AvatarFallback className="bg-sky-500 text-white">
+                        {avatarFallback}
                     </AvatarFallback>
                 </Avatar>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="center" side="right" className="w-60">
-                <DropdownMenuItem>
-                    
+                <DropdownMenuItem onClick={() => signOut()} className="h-10" >
+                    <LogOut className="size-4 mr-2" />
+                    Log out
                 </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
