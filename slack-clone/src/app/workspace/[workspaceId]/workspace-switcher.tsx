07e4cdd73724c75d98b02467cab90bbaf1e89751
Michael Van <michaelvan996@gmail.com>
2024-09-05 23:59:21 -0400

    update

--- slack-clone/src/app/workspace/[workspaceId]/workspace-switcher.tsx
+++ slack-clone/src/app/workspace/[workspaceId]/workspace-switcher.tsx
@@ -1,7 +1,24 @@
+import { Button } from "@/components/ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu";
+  
+
 export const WorkspaceSwitcher = () => {
     return (
-        <div>
-            Workspace switcher
-        </div>
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <Button className="size-9 relative overflow-hidden bg-[#ABABAD] hover:bg-[#ABABAD]/80 text-slate-800 font-semibold text-xl">
+                    A
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent side="bottom" align="start" className="w-64">
+
+            </DropdownMenuContent>
+        </DropdownMenu>
     );
 };
